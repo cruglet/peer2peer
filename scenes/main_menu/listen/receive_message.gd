111aa90044dc4ff67d6e1f9946f962713d0dad57
cruglet <cruglet@gmail.com>
2025-08-29 07:28:07 -0400

    werf

--- scenes/main_menu/listen/receive_message.gd
+++ scenes/main_menu/listen/receive_message.gd
@@ -1,7 +1,12 @@
 extends Node
 
+@export var main_chat_scroller: ScrollContainer 
+
+
 func _ready() -> void:
 	P2P.incoming_message.connect(received_message)
-	
-func received_message(user_id: int, raw_message: PackedByteArray) -> void:
-	var message: String = raw_message.get_string_from_utf8()
+
+
+func received_message(_user_id: int, _raw_message: PackedByteArray) -> void:
+	await get_tree().create_timer(0.5).timeout
+	main_chat_scroller.scroll_vertical = 999999999
